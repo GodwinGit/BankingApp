fc86c451dac5ec74a325cfb1f1cbbf2a62bc5b00
Godwin <Godwinadeleke@gmail.com>
2018-04-18 14:57:26 +0100

    Json incomplete

--- src/main/java/accountManagement/Account.java
+++ src/main/java/accountManagement/Account.java
@@ -1,5 +1,34 @@
 package accountManagement;
 
 public class Account {
+	private String firstName;
+	private String lastName;
+	private final int accountNumber;
+	
+	public Account(int accountNumber, String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.accountNumber = accountNumber;
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
 }
--- src/main/java/accountManagement/Service.java
+++ src/main/java/accountManagement/Service.java
@@ -1,5 +1,38 @@
 package accountManagement;
+import java.util.HashMap;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Service {
+	private HashMap<Integer,Account> accounts = new HashMap<Integer,Account>();
+	
+	public HashMap<Integer, Account> getAccountsMap() {
+		return accounts;
+	}
 
+	public Account getAccount(int accountNumber) {
+		return accounts.get(accountNumber);
+	}
+	
+	public void addAccount(Account account) {
+		accounts.put(account.getAccountNumber(),account);
+	}
+	
+	public void removeAccount(int accountNumber) {
+		boolean accountExists = accounts.containsKey(accountNumber);
+		if (accountExists) {
+			accounts.remove(accountNumber);
+		}
+	}
+	
+	public String mapToJSON() {
+		ObjectMapper jsonMaker = new ObjectMapper();
+		try {
+			return jsonMaker.writeValueAsString(accounts);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return "Error";
+		}
+	}
+	
 }
